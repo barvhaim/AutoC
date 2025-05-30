b1ff0c44addc8c8650a6150977fbb5cfa1a8951e
bar haim <barha@il.ibm.com>
2025-05-31 01:49:57 +0300

    vt link

---
+++ frontend/src/utils/strings.ts
@@ -0,0 +1,9 @@
+export const cleanIocValue = (value: string): string => {
+  // Replace "[.]" with "." from the value of urls and IPs
+  let cleanedValue = value.replace(/\[.\]/g, ".");
+  /// Replace "hxxps://" with "https://" in the cleaned value
+  cleanedValue = cleanedValue.replace(/hxxps:\/\//g, "https://");
+  // Replace "hxxp://" with "http://" in the cleaned value
+  cleanedValue = cleanedValue.replace(/hxxp:\/\//g, "http://");
+  return cleanedValue;
+};
